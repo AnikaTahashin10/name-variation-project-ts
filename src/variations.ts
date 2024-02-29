c361361ec9ccf3cbbf9a3e252fb99e2bd1b44eb3
AnikaTahashin10 <anikatahashin90@gmail.com>
2024-02-29 23:38:13 +0200

    completed

---
+++ src/variations.ts
@@ -0,0 +1,26 @@
+export function getNumberOfChars(name: string) {
+  // number of characters in: name
+  return (name.length )
+}
+
+export function getFirstChar(name: string) {
+  // first character of: name
+return name.charAt(0);
+}
+
+export function getLastChar(name: string) {
+  // last character of: name
+  return name.charAt(name.length - 1);
+}
+
+export function getLower(name: string) {
+  // name all in lower case (example: "ABC" becomes "abc")
+  return (name.toLowerCase() )
+
+}
+
+export function getUpper(name: string) {
+  // name all in upper case (example: "abc" becomes "ABC")
+  return (name.toUpperCase() )
+
+}
